f737ecf30b598cf0d7056f2eedb439789b2c4342
Christopher II Lajom <penncorleone@gmail.com>
2021-06-16 19:08:44 +0800

    Initalized template styles and content.

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,15 @@
-import React from "react";
+import styles from "../styles/Main.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Assessment Application</h1>
+      <div className={styles.imageContainer}>
+        <img
+          src="https://picsum.photos/id/237/220/290?grayscale"
+          className={styles.image}
+        />
+      </div>
     </div>
   );
 };
