9b7e1591bb0913e0bdbf20100f030a4150fd0728
Christopher II Lajom <penncorleone@gmail.com>
2021-06-16 15:12:58 +0800

    Added function that redirects to signin page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    router.push("/signin");
+  };
+
   return (
     <div className={styles.container}>
-      <form noValidate className={styles.form}>
+      <form noValidate className={styles.form} onSubmit={handleSubmit}>
         <h1 className={styles.title}>Sign Up</h1>
 
         <input type="email" placeholder="Email:" className={styles.input} />
