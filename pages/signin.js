9ace0537b070110414ab3a681c6734824e832e85
Christopher II Lajom <penncorleone@gmail.com>
2021-06-16 17:41:34 +0800

    Added some template styles from other page.

--- pages/signin.js
+++ pages/signin.js
@@ -1,9 +1,30 @@
-import React from "react";
+import styles from "../styles/SignIn.module.css";
+import { useRouter } from "next/router";
 
 const SignIn = () => {
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    router.push("/signin");
+  };
+
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className={styles.container}>
+      <form noValidate className={styles.form} onSubmit={handleSubmit}>
+        <h1 className={styles.title}>Sign In</h1>
+
+        <input type="email" placeholder="Email:" className={styles.input} />
+        <input
+          type="password"
+          placeholder="Password:"
+          className={styles.input}
+        />
+        <button type="submit" className={styles.submitBtn}>
+          Login
+        </button>
+      </form>
     </div>
   );
 };
