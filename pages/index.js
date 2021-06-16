54cabdf718d41d214077fc14e514c48815cc0133
Christopher II Lajom <penncorleone@gmail.com>
2021-06-16 13:47:22 +0800

    Initialized SignUp Page with minimum styling.

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className={styles.container}>
+      <form noValidate className={styles.form}>
+        <h1 className={styles.title}>Sign Up</h1>
+
+        <input type="email" placeholder="Email:" className={styles.input} />
+        <input type="text" placeholder="Password:" className={styles.input} />
+        <button type="submit" className={styles.submitBtn}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
