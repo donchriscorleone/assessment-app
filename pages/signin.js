e5612de4ad9bc9b3ae4bca919a913fe64c75253d
Christopher II Lajom <penncorleone@gmail.com>
2021-06-17 12:14:04 +0800

    Added toast and simple validation on sign in page.

--- pages/signin.js
+++ pages/signin.js
@@ -1,31 +1,87 @@
 import styles from "../styles/SignIn.module.css";
 import { useRouter } from "next/router";
+import Toast from "../components/toast";
+import { useState } from "react";
 
 const SignIn = () => {
   const router = useRouter();
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [toastInfo, setToastInfo] = useState({
+    message: "",
+    type: "",
+    visible: false,
+  });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    router.push("/home");
+    if (!user.email) {
+      setToastInfo({
+        message: "Please input your email",
+        type: "danger",
+        visible: true,
+      });
+
+      setTimeout(() => {
+        setToastInfo({ ...toastInfo, visible: false });
+      }, 3000);
+      return;
+    }
+
+    if (!user.password) {
+      setToastInfo({
+        message: "Please fill your password",
+        type: "danger",
+        visible: true,
+      });
+      setTimeout(() => {
+        setToastInfo({ ...toastInfo, visible: false });
+      }, 3000);
+      return;
+    }
+
+    setToastInfo({
+      message: "Redirecting to login",
+      type: "success",
+      visible: true,
+    });
+
+    setTimeout(() => {
+      router.push("/home");
+    }, 2500);
   };
 
   return (
-    <div className={styles.container}>
-      <form noValidate className={styles.form} onSubmit={handleSubmit}>
-        <h1 className={styles.title}>Sign In</h1>
+    <>
+      {toastInfo.visible && (
+        <Toast message={toastInfo.message} type={toastInfo.type} />
+      )}
+      <div className={styles.container}>
+        <form noValidate className={styles.form} onSubmit={handleSubmit}>
+          <h1 className={styles.title}>Sign In</h1>
 
-        <input type="email" placeholder="Email:" className={styles.input} />
-        <input
-          type="password"
-          placeholder="Password:"
-          className={styles.input}
-        />
-        <button type="submit" className={styles.submitBtn}>
-          Login
-        </button>
-      </form>
-    </div>
+          <input
+            type="email"
+            placeholder="Email:"
+            className={styles.input}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password:"
+            className={styles.input}
+            onChange={handleChange}
+          />
+          <button type="submit" className={styles.submitBtn}>
+            Login
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
