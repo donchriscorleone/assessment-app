4087060af6de7b46b93ab03aa75302c445bb0592
Christopher II Lajom <penncorleone@gmail.com>
2021-06-17 14:33:54 +0800

    Added some animation in picsum component.

--- components/Picsum.js
+++ components/Picsum.js
@@ -2,15 +2,17 @@ import React from "react";
 import styles from "../styles/Picsum.module.css";
 
 const data = [
-  "https://picsum.photos/seed/2/200/300?grayscale",
-  "https://picsum.photos/seed/2/200/300?grayscale",
+  "https://picsum.photos/seed/2/120/150?grayscale",
+  "https://picsum.photos/seed/10/120/150?grayscale",
+  "https://picsum.photos/seed/20/120/150?grayscale",
+  "https://picsum.photos/seed/30/120/150?grayscale",
 ];
 
 const Picsum = () => {
   return (
     <section className={styles.pictures}>
       {data.map((img, index) => {
-        return <img src={img} alt={index} key={index} />;
+        return <img src={img} alt={index} key={index} className={styles.img} />;
       })}
     </section>
   );
